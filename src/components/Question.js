e9cbf926911b5b9ef15e5bd82e63625a4f9bd668
Gavin Lang <gavinl@github.com>
2020-05-26 20:46:28 +1000

    initial question reducer

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useReducer, useEffect } from "react";
+import { actionTypes, reducer } from "../reducers/questionnaireReducer";
 
-const Question = ({ question }) => {
-  return <pre>{JSON.stringify(question)}</pre>;
+const Question = (props) => {
+    const question = props.item;
+    const [state, dispatch] = useReducer(reducer, {
+        questionnaire: question,
+        questions: [],
+    });
+
+    useEffect(() => {
+        dispatch({ type: actionTypes.registerQuestion, payload: question });
+    }, question);
+
+    return <pre>{JSON.stringify(question)}</pre>;
 };
 
 export default Question;
