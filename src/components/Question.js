21468659a3b065a75ca49022c000d8a4777da53d
GavinL <gavinl@github.com>
2020-07-05 14:46:18 +1000

    tweak styles for unhandled questions

--- src/components/Question.js
+++ src/components/Question.js
@@ -6,28 +6,29 @@ import FhirChoice from "./FhirChoice";
 import "./Question.scss";
 
 const Question = (props) => {
-    const question = props.item;
+  const question = props.item;
 
-    switch (question.type) {
-        case "text":
-            return <FhirText question={question}/>;
+  switch (question.type) {
+    case "text":
+      return <FhirText question={question} />;
 
-        case "choice":
-            return <FhirChoice question={question}/>;
+    case "choice":
+      return <FhirChoice question={question} />;
 
-        default:
-            return (
-                <div className="unhandled-question-type">
-                  <h3>{question.type} <span className="question-link-id">{question["linkId"]}</span></h3>
-                    <pre>
-                        {JSON.stringify(question, null, 2)}
-                    </pre>
-                </div>
-            );
-    }
+    default:
+      return (
+        <div className="unhandled-question">
+          <div className="header">
+            <span className="type">{question.type}</span>{" "}
+            <span className="link-id">{question["linkId"]}</span>
+          </div>
+          <pre className="raw">{JSON.stringify(question, null, 2)}</pre>
+        </div>
+      );
+  }
 };
 
 Question.propTypes = {
-    item: PropTypes.object.isRequired,
+  item: PropTypes.object.isRequired,
 };
 export default Question;
